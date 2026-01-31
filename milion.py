8d4a641b30193345f1295991908a8132f34b9928
ilai <lifeingeo@gmail.com>
2026-01-31 10:24:10 +0300

    I need the actual code changes (diff) for `milion.py` to generate an accurate commit message.

--- milion.py
+++ milion.py
@@ -1,4 +1,11 @@
 print("столица-германи?")
-a =['Берлин', 'Вена', 'Брюссель', 'Лиссабон'] 
+a =['A. Берлин', 'B. Вена', 'C. Брюссель', 'D. Лиссабон'] 
+right_answer = 'A'
 for i in range (len(a)):
     print(a[i])
+answer = input("Ваш ответ?: ")
+if answer == right_answer:
+    print("Правильный ответ!")
+else:
+    print("Неправильный ответ!")
+
